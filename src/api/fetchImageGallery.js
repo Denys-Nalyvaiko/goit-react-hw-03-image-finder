183b5648f169c842643851df92e92dbac2ffaeec
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-11-21 13:12:06 +0100

    optimize load more button

--- src/api/fetchImageGallery.js
+++ src/api/fetchImageGallery.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '39895100-b1bc415b383dfc0e1a37c2dc7';
 
-export const fetchImageGallery = async (query, page) => {
+export const fetchImageGallery = async (query, page, perPage) => {
   const responce = await axios.get(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&page=${page}&per_page=12&image_type=photo&orientation=horizontal`
+    `${BASE_URL}?key=${API_KEY}&q=${query}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal`
   );
 
   return responce.data;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,19 +12,49 @@ export class App extends Component {
   };
 
   page = 1;
+  perPage = 12;
+  isHidden = true;
 
   handleQueryFormSubmit = async query => {
-    this.setState({ query });
-    const { hits } = await fetchImageGallery(query, this.page);
-    this.setState({
-      images: hits,
-    });
+    this.page = 1;
+
+    try {
+      this.setState({ query });
+      const { hits, totalHits } = await fetchImageGallery(
+        query,
+        this.page,
+        this.perPage
+      );
+      this.setState({
+        images: hits,
+      });
+
+      if (this.page * this.perPage < totalHits) {
+        this.isHidden = false;
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleLoadMoreBtnClick = async () => {
     this.page += 1;
-    const { hits } = await fetchImageGallery(this.state.query, this.page);
-    this.setState(prevState => ({ images: [...prevState.images, ...hits] }));
+
+    try {
+      const { hits, totalHits } = await fetchImageGallery(
+        this.state.query,
+        this.page,
+        this.perPage
+      );
+
+      this.setState(prevState => ({ images: [...prevState.images, ...hits] }));
+
+      if (this.page * this.perPage > totalHits) {
+        this.isHidden = true;
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
@@ -34,7 +64,9 @@ export class App extends Component {
       <Container>
         <Searchbar onSubmit={this.handleQueryFormSubmit} />
         {Boolean(images.length) && <ImageGallery images={images} />}
-        <LoadMoreButton onClick={this.handleLoadMoreBtnClick} />
+        {!this.isHidden && (
+          <LoadMoreButton onClick={this.handleLoadMoreBtnClick} />
+        )}
       </Container>
     );
   }
