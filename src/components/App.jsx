0213b7b8f2676e6c15b8cfaee61edf847417b871
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-11-21 21:53:29 +0100

    refactoring load more button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { CirclesWithBar } from 'react-loader-spinner';
 import { Container } from './Container/Container.styled';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -9,28 +10,40 @@ export class App extends Component {
   state = {
     query: '',
     images: [],
+    isLoading: false,
   };
 
   page = 1;
   perPage = 12;
-  isHidden = true;
+  isButtonHidden = true;
+
+  componentDidUpdate(_, prevState) {
+    prevState.query !== this.state.query && this.setState({ isLoading: true });
+
+    prevState.images !== this.state.images &&
+      this.page === 1 &&
+      this.setState({ isLoading: false });
+  }
 
   handleQueryFormSubmit = async query => {
     this.page = 1;
+    this.setState({ query });
 
     try {
-      this.setState({ query });
       const { hits, totalHits } = await fetchImageGallery(
         query,
         this.page,
         this.perPage
       );
+
       this.setState({
         images: hits,
       });
 
-      if (this.page * this.perPage < totalHits) {
-        this.isHidden = false;
+      this.isButtonHidden = false;
+
+      if (this.page * this.perPage > totalHits) {
+        this.isButtonHidden = true;
       }
     } catch (error) {
       console.log(error);
@@ -39,6 +52,7 @@ export class App extends Component {
 
   handleLoadMoreBtnClick = async () => {
     this.page += 1;
+    this.isButtonHidden = true;
 
     try {
       const { hits, totalHits } = await fetchImageGallery(
@@ -48,9 +62,10 @@ export class App extends Component {
       );
 
       this.setState(prevState => ({ images: [...prevState.images, ...hits] }));
+      this.isButtonHidden = false;
 
       if (this.page * this.perPage > totalHits) {
-        this.isHidden = true;
+        this.isButtonHidden = true;
       }
     } catch (error) {
       console.log(error);
@@ -58,13 +73,29 @@ export class App extends Component {
   };
 
   render() {
-    const { images } = this.state;
+    const { images, isLoading } = this.state;
 
     return (
       <Container>
         <Searchbar onSubmit={this.handleQueryFormSubmit} />
-        {Boolean(images.length) && <ImageGallery images={images} />}
-        {!this.isHidden && (
+        {isLoading && (
+          <CirclesWithBar
+            height="100"
+            width="100"
+            color="#3f51b5"
+            wrapperStyle={{ margin: '24px auto 0' }}
+            wrapperClass=""
+            visible={true}
+            outerCircleColor=""
+            innerCircleColor=""
+            barColor=""
+            ariaLabel="circles-with-bar-loading"
+          />
+        )}
+        {Boolean(images.length) && !isLoading && (
+          <ImageGallery images={images} />
+        )}
+        {!this.isButtonHidden && !isLoading && (
           <LoadMoreButton onClick={this.handleLoadMoreBtnClick} />
         )}
       </Container>
